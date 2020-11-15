4d5d77dfa85bc504b7b2fc41c748335eecc5dc64
Tran Thi Hong Nhung <nhung20001101@gmail.com>
2020-11-15 15:47:11 +0700

    update2

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
 
-app.get('/', function (req, res, ext) {
-  res.sendFile(__dirname + "/public/index.html");
+app.all('*', function(req, res, next){
+  res.header('Access-Control-Allow-Origin',  '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With, yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS'){
+    res.send(200);
+  }
+  else{
+    next();
+  }
 });
 
-app.post('/', function (req, res, ext) {
+app.get('/', function (req, res, ext) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
