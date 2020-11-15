312bffb9418dc22e08e33ab9e2fae9b4f76da049
Tran Thi Hong Nhung <nhung20001101@gmail.com>
2020-11-15 11:31:36 +0700

    make it better

---
+++ public/client.js
@@ -0,0 +1,21 @@
+var socket = io.connect('http://localhost:8000');
+socket.on('connect', function (data) {
+  socket.emit('join', 'A clien is joining chatroom');
+});
+
+socket.on('chat', function (data) {
+  $('#chatArea').append('<p> <b>' + data.username + '</b>:  ' + data.message + '</li>')
+});
+
+$('form').submit(function () {
+  var username = $("#username").val();
+  var message = $('#message').val();
+  if (username != "") {
+    socket.emit('messages', { username: username, message: message });
+    $("#message").val("");
+  } else {
+    alert("Enter your name!");
+  }
+
+  return false;
+});
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+var server = require('http').createServer(app);
+
+var io = require('socket.io')(server);
+
+app.get('/', function (req, res, ext) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(express.static('public'));
+
+io.on('connection', function (client) {
+  console.log('Client sended...');
+  client.on('join', function (data) {
+    console.log(data);
+  });
+  client.on('messages', function (data) {
+    client.emit('chat', data);
+    client.broadcast.emit('chat', data);
+  });
+});
+server.listen(8000);
